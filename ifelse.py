3b11b138cce596464eabbe0b8ff9148a61075a6c
be-sohelz <be.sohelz@gmail.com>
2020-06-13 18:57:33 +0530

    Next Version

---
+++ ifelse.py
@@ -0,0 +1,71 @@
+#simple if else
+# a=20
+# b=3
+# if a<b:
+#     print(a)
+# else:
+#     print(b)
+
+
+# Empty or not
+# while True:
+#     name=input('Please  enter name')
+#     if name:
+#         pass
+#     else:
+#         print("you didn't enter your name")
+
+
+# Break
+# for i in range(11):
+#     if i==5:
+#         break
+#     print(i)
+
+
+# continue
+# for i in range(11):
+#     if i in range(2,5):
+#         continue
+#     print(i)
+
+
+
+# Number Guessing Game
+# import random
+# win_num=random.randint(1,10)
+# count=0
+# while True:
+#     guess=int(input('enter guess num:  '))
+#     if guess==win_num:
+#         print('you win')
+#         break
+#     elif guess<win_num:
+#         print('too low')
+#     else:
+#         print('too high')
+#     count+=1
+#     print(f'you tried {count} times')
+
+
+
+
+# And & Or Operator
+
+# and operator
+# name='abc'
+# age=20
+# if name =='abc' and age==20:
+#     print('You Are Eligible')
+# else:
+#     print('You Are Under Age')
+
+
+
+# or operator
+# name='abc'
+# age=20
+# if name =='abc' or age==15:
+#     print('You Are Eligible')
+# else:
+#     print('You Are Under Age')
