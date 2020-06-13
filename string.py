444f988a6ed5327e4f5961b1baad960d9bfa0998
be-sohelz <be.sohelz@gmail.com>
2020-06-13 18:43:04 +0530

    First Commit

---
+++ string.py
@@ -0,0 +1,34 @@
+name='ssoohheells'
+
+# # replace method
+# print(name.replace('s','S',1))
+# print(name)
+
+print(name[0:-1:2])
+
+# l=len(name)
+# print(l)
+
+# n=name.lower()
+# print(n)
+# print(name.upper())
+# print(name.title())
+
+# # find method / find position
+# f=name.find('s',2)
+# print(f)
+
+# replace method
+# r=name.replace('s','o')
+# print(r)
+
+
+# # center method
+# k=name.center(15,'*')
+# print(k)
+
+
+#multiple variable input
+# name,age=input('enter name , age ').split(',')
+# print(name)
+# print(age)
