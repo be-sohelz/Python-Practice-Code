fa2f9581483a3dfac2ac9e5aaeb3a9bba17e2063
be-sohelz <be.sohelz@gmail.com>
2020-06-14 15:35:40 +0530

    Pactice Version

---
+++ ex_prac.py
@@ -0,0 +1,10 @@
+# print("Twinkle, twinkle, little star,\n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+# import sys
+# print(sys.version)
+# print(sys.version_info)
+
+import datetime
+now=datetime.datetime.now()
+print(now)
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
