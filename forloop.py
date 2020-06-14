0abe49d7018ea36e0375c5bf4c087a83353445c4
be-sohelz <be.sohelz@gmail.com>
2020-06-14 15:39:28 +0530

    Forloop Python

---
+++ forloop.py
@@ -0,0 +1,73 @@
+#addtion of number
+# total=0
+# for i in range(11):
+#     total+=i
+#     print(f'{i}  :  {total} ')
+
+
+#addtion of number_string
+# number=input('enter a number')
+# total=0
+# i=1
+# for i in range(len(number)):
+#     total+=int(number[i])
+# print(total)
+
+
+#count of charector in string
+# name=input('enter your string')
+# temp=' '
+# for i in range(len(name)):
+#     if name[i] not in temp:
+#         temp+=name[i]
+#         print(f'char {name[i]} :  {name.count(name[i])}')
+
+
+# Step Argument
+# for i in range(1,10,2): ##for print 1 to 10 
+#     print(i)
+
+
+
+#for loops for universal language
+# name='sohel' 
+# for i in range(len(name)):
+#     print(name[i])
+
+
+
+
+#for loops for python language
+# name='sohel'
+# for i in name:
+#     print(i)
+  
+
+# for universal
+# number="123"
+# total=0
+# for i in range(len(number)):
+#     total+=int(number[i])
+# print(total)
+    
+
+# for loop for addition of index in python
+# number=input("enter number")
+# total=0
+# for i in number:
+#     total=total+int(i)
+# print(total)
+
+
+# #pattern program 
+# for num in range(10):
+#     for i in range(num):
+#         print(num,end=" ")
+#     print('\n')    
+
+# num='12345'
+# total=0
+# i=1
+# for i in range(len(num)):
+#     total+=int(num[i])
+# print(total)
